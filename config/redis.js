f429f1ff8f169d952fafd62a0878c28790ed04bd
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2026-02-05 13:05:27 +0100

    edited config/redis.js to use render/upstash redis

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,7 @@
 const IORedis = require("ioredis");
 
-const redisConnection = new IORedis({
+const redisConnection = new IORedis(
+  process.env.UPSTASH_REDIS_URL || {
   host: "127.0.0.1",
   port: 6379,
   maxRetriesPerRequest: null, //as required bu bullmq v5
